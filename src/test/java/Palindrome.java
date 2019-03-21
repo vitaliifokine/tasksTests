a0c294df3dafba4a05879ec4496ab0c2725f6037
vitaliifokine <Vf281992>
2019-03-20 21:58:28 -0400

    Palindrome

--- src/test/java/Palindrome.java
+++ src/test/java/Palindrome.java
@@ -1,2 +1,18 @@
 public class Palindrome {
+
+    public static void main(String [ ] args) {
+        System.out.println(isPalindrom("radar"));
+    }
+    
+    static boolean isPalindrom(String str) {
+        if (str.length() == 0 || str.length() == 1){
+            return true;
+        } else {
+            if (str.charAt(0) != str.charAt(str.length() - 1)){
+                return false;
+            } else {
+                return isPalindrom(str.substring(1, str.length()-1));
+            }
+        }
+    }
 }
