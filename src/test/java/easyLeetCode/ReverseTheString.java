c7d705160e0394abc964f00676bd359deb3925e3
Vitalii Fokin <vitaliiifokine@gmail.com>
2019-06-12 23:02:01 -0400

    Stone and DiamondsII

--- src/test/java/easyLeetCode/ReverseTheString.java
+++ src/test/java/easyLeetCode/ReverseTheString.java
@@ -5,27 +5,51 @@ public class ReverseTheString {
 
     public static void main(String[] args) {
         System.out.println(getReversedString("Zelenskiy"));
+        char[] vol = {'V', 'o', 'l'};
+        System.out.println(getRecursivelyEncapsulated("Volodymyr"));
     }
 
-    public static String getReversedString(String input){
+    public static String getReversedString(String input) {
         // String to Array
         char data[] = new char[input.length()];
-        for (int i=0; i<input.length(); i++){
-            data[i]=input.charAt(i);
+        for (int i = 0; i < input.length(); i++) {
+            data[i] = input.charAt(i);
         }
-
-        for (int i=0; i<data.length/2; i++){
+        for (int i = 0; i < data.length / 2; i++) {
             char temp = data[i];
-            data[i]=data[data.length-1-i];
-            data[data.length-1-i]=temp;
+            data[i] = data[data.length - 1 - i];
+            data[data.length - 1 - i] = temp;
         }
         StringBuilder stringBuilder = new StringBuilder();
         for (int i = 0; i < data.length; i++) {
             stringBuilder.append(data[i]);
         }
+        return stringBuilder.toString();
+    }
 
+    private static char[] getRecursively(char[] arr, int counter) {
+        counter++;
+        char temp = arr[counter];
+        arr[counter] = arr[arr.length - 1 - counter];
+        arr[arr.length - 1 - counter] = temp;
+
+        if (counter < arr.length / 2) {
+            getRecursively(arr, counter);
+            return arr;
+        }
+        return arr;
+    }
+
+    private static String getRecursivelyEncapsulated(String string) {
+        char data[] = new char[string.length()];
+        for (int i = 0; i < string.length(); i++) {
+            data[i] = string.charAt(i);
+        }
+        getRecursively(data, -1);
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < data.length; i++) {
+            stringBuilder.append(data[i]);
+        }
         return stringBuilder.toString();
     }
 }
-
-
