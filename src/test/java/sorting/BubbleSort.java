6d021ed83663779c4f9a688b67e2039f667f506a
Vitalii Fokin <vitaliiifokine@gmail.com>
2019-06-13 19:34:32 -0400

    Updates

--- src/test/java/sorting/BubbleSort.java
+++ src/test/java/sorting/BubbleSort.java
@@ -1,4 +1,24 @@
 package sorting;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class BubbleSort {
+    public static void main(String[] args) {
+        int[]array={6,8,45,1};
+        System.out.println(Arrays.toString(bubbleSort(array)));
+    }
+
+    static int[] bubbleSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++)
+            for (int j = 0; j < n - i - 1; j++)
+                if (arr[j] > arr[j + 1]) {
+                    // swap temp and arr[i]
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+        return arr;
+    }
 }
