b91289f3c732f6d75f42a39c7e12b32cacafd8b2
Vitalii Fokin <vitaliiifokine@gmail.com>
2019-08-24 12:14:42 -0400

    Updates IIII

--- src/test/java/palindromes/Palindrome.java
+++ src/test/java/palindromes/Palindrome.java
@@ -6,6 +6,9 @@ public class Palindrome {
         System.out.println(isPalindrom("pop"));
     }
 
+
+    // Elegant show of recursion in the end. The substring make
+    // a input string less and less till moment of ending of recursion
     static boolean isPalindrom(String str) {
         str = str.toLowerCase();
         if (str.length() == 0 || str.length() == 1) {
--- src/test/java/strings/StringFirstLetterToUpperCase.java
+++ src/test/java/strings/StringFirstLetterToUpperCase.java
@@ -1,33 +1,24 @@
 package strings;
 
+import java.util.Arrays;
+
 public class StringFirstLetterToUpperCase {
 
-    static String convert(String str)
-    {
-        // Create a char array of given String
-        char ch[] = str.toCharArray();
-        for (int i = 0; i < str.length(); i++) {
-            // If first character of a word is found
-            if (i == 0 && ch[i] != ' ' ||
-                    ch[i] != ' ' && ch[i - 1] == ' ') {
-                // If it is in lower-case
-                if (ch[i] >= 'a' && ch[i] <= 'z') {
-                    // Convert into Upper-case
-                    ch[i] = (char)(ch[i] - 'a' + 'A');
-                }
+    static String convert(String str) {
+        char [] data = new char[str.length()];
+        for (int i=0; i<str.length(); i++){
+            data[i] = str.charAt(i);
+            if (String.valueOf(data[i])==" "){
+                data[i+1] = Character.toUpperCase(data[i+1]);
             }
-
-            // If apart from first character
-            // Any one is in Upper-case
-            else if (ch[i] >= 'A' && ch[i] <= 'Z')
-
-                // Convert into Lower-Case
-                ch[i] = (char)(ch[i] + 'a' - 'A');
+        }
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i =0; i<data.length;i++){
+            stringBuilder.append(data[i]);
         }
 
-        // Convert the char array to equivalent String
-        String st = new String(ch);
-        return st;
+        return stringBuilder.toString();
+
     }
 
     public static void main(String[] args)
