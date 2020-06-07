ef255660b1e01658bb0b86a004d814ac5d16425f
Vitalii Fokin <vitaliiifokine@gmail.com>
2020-06-07 17:27:45 -0400

    Updates with LeetCode tasks II
    Signed-off-by: Vitalii Fokin <vitaliiifokine@gmail.com>

---
+++ src/test/java/romanToArabic/Main.java
@@ -0,0 +1,42 @@
+package romanToArabic;
+
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+public class Main {
+
+    public static int romanToArabic(String input) {
+        String romanNumeral = input.toUpperCase();
+        int result = 0;
+
+        List<RomanNumeral> romanNumerals = RomanNumeral.getReverseSortedValues();
+
+        int i = 0;
+
+        while ((romanNumeral.length() > 0) && (i < romanNumerals.size())) {
+            RomanNumeral symbol = romanNumerals.get(i);
+            if (romanNumeral.startsWith(symbol.name())) {
+                result += symbol.getValue();
+                romanNumeral = romanNumeral.substring(symbol.name().length());
+            } else {
+                i++;
+            }
+        }
+
+        if (romanNumeral.length() > 0) {
+            throw new IllegalArgumentException(input + " cannot be converted to a Roman Numeral");
+        }
+
+        return result;
+    }
+
+    @Test
+    public void given2018Roman_WhenConvertingToArabic_ThenReturn2018() {
+        String roman2018 = "IV";
+
+        int result = romanToArabic(roman2018);
+
+        System.out.println(result);
+    }
+}
